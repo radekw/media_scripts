97aa50eee082d4fa342b2210715ef5eabbbc4427
Radek Wierzbicki <rwierzbicki@conductor.com>
2010-04-28 13:35:55 -0400

    licese and readme

--- src/save_tv.py
+++ src/save_tv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright [2010] [Radek Wierzbicki]
+# Copyright 2010 Radek Wierzbicki
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
